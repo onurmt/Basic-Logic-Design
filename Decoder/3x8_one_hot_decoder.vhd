68a8d7a05d9c0f26ad086eb3d4ae349333f957c5
Onur Mete <55415860+onurmt@users.noreply.github.com>
2022-02-11 03:35:51 +0300

    Update 3x8_one_hot_decoder.vhd

--- Decoder/3x8_one_hot_decoder.vhd
+++ Decoder/3x8_one_hot_decoder.vhd
@@ -10,14 +10,15 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity decoder_3x8 is
 port(
 		abc : in std_logic_vector(2 downto 0);
-		-- Output
-		f   : out std_logic_vector(7 downto 0)
-		
-	);
+	
+		 -- Output
+		f   : out std_logic_vector(7 downto 0)	
+     );
 end entity ;
 
 architecture arch of decoder_3x8 is
 begin 
+	
 	  f <= "00000001" when (abc = "000") else 
 	       "00000010" when (abc = "001") else 
 	       "00000100" when (abc = "010") else 
@@ -25,6 +26,5 @@ begin
 	       "00010000" when (abc = "100") else 
 	       "00100000" when (abc = "101") else 
 	       "01000000" when (abc = "110") else 
-	       "10000000" when (abc = "111"); 	   
-		       
+	       "10000000" when (abc = "111"); 	   		       
 end architecture;
