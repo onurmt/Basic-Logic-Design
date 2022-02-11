fec418f1d3e974e310ea07aa20cad29488714069
Onur Mete <55415860+onurmt@users.noreply.github.com>
2022-02-11 03:25:16 +0300

    Update reg_8_bit.vhd

--- Register/reg_8_bit.vhd
+++ Register/reg_8_bit.vhd
@@ -6,12 +6,13 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all; 
 
 entity reg_8_bit is 
-	port (	clk : in std_logic; 
-		    rst : in std_logic; 
+	port (	
+	    clk : in std_logic; 
+            rst : in std_logic; 
          reg_in : in std_logic_vector(7 downto 0); 
              EN : in std_logic; 
 			 
-			-- Output
+        -- Output
         reg_out : out std_logic_vector(7 downto 0)
 		); 
 end entity; 
@@ -21,7 +22,7 @@ architecture arch of reg_8_bit  is
 		reg_proc : process (clk, rst)
 	begin 
 		if (rst = '0') then
-			reg_out <= x"00"; 
+	       reg_out <= x"00"; 
 		elsif (clk'event and clk = '1') then 
 			if (EN = '1') then 
 			reg_out <= reg_in; 
