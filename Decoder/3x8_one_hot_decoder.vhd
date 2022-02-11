980559d00886d1c495a366949264dd1ad8d493bf
Onur Mete <55415860+onurmt@users.noreply.github.com>
2022-02-11 03:20:29 +0300

    Update 3x8_one_hot_decoder.vhd

--- Decoder/3x8_one_hot_decoder.vhd
+++ Decoder/3x8_one_hot_decoder.vhd
@@ -18,14 +18,13 @@ end entity ;
 
 architecture arch of decoder_3x8 is
 begin 
-
 	  f <= "00000001" when (abc = "000") else 
-		   "00000010" when (abc = "001") else 
-		   "00000100" when (abc = "010") else 
-		   "00001000" when (abc = "011") else 
-		   "00010000" when (abc = "100") else 
-		   "00100000" when (abc = "101") else 
-		   "01000000" when (abc = "110") else 
-		   "10000000" when (abc = "111"); 
-		   
+	       "00000010" when (abc = "001") else 
+	       "00000100" when (abc = "010") else 
+	       "00001000" when (abc = "011") else 
+	       "00010000" when (abc = "100") else 
+	       "00100000" when (abc = "101") else 
+	       "01000000" when (abc = "110") else 
+	       "10000000" when (abc = "111"); 	   
+		       
 end architecture;
