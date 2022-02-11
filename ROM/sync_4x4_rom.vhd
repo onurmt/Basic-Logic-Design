fc68b54f62e2e51ec6ccb46769c63023f657fbd7
Onur Mete <55415860+onurmt@users.noreply.github.com>
2022-02-11 03:23:55 +0300

    Update sync_4x4_rom.vhd

--- ROM/sync_4x4_rom.vhd
+++ ROM/sync_4x4_rom.vhd
@@ -8,7 +8,7 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity sync_4x4_rom is 
     port( 
-			 clk : in std_logic; 
+		     clk : in std_logic; 
 		 address : in std_logic_vector(1 downto 0); 
 		 -- Output
 		data_out : out std_logic_vector(3 downto 0)
@@ -20,10 +20,10 @@ architecture arch of sync_4x4_rom is
 type ROM_type is array (0 to 3) of std_logic_vector(3 downto 0); 
 
 constant ROM : ROM_type := (0 => "1110", 
-							1 => "0010", 
-							2 => "1111", 
-							3 => "0100"
-						    ); 
+			    1 => "0010", 
+			    2 => "1111", 
+			    3 => "0100"
+			    ); 
 begin 
 	Memory_proc : process (clk) 
 	begin 
